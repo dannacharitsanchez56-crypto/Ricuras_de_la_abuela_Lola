c252d0058c73f6f6d05b789f582a7061de30aeb6
Danna Charit Sanchez <dannacharitsanchez56@gmail.com>
2026-09-13 19:55:21 -0500

    Primer commit: proyecto Las Ricuras de la Abuela Lola

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('historia/', views.historia, name='historia'),
+    path('menu/', views.menu, name='menu'),
+    path('reservas/', views.reservas, name='reservas'),
+    path('Platos/', views.galeria, name='Platos'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('login/', views.login_view, name='login'),
+    path('registro/', views.registro, name='registro'),
+    path('perfil/', views.perfil, name='perfil'),
+]
