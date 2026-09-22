db963d48e59a4de812cf9bae9f17b86948752ba2
Danna Charit Sanchez <dannacharitsanchez56@gmail.com>
2026-09-21 19:00:35 -0500

    Mejoro el diseño con iconos y arreglo login

--- las_ricuras/models.py
+++ las_ricuras/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Plato(models.Model):
+    CATEGORIAS = [
+        ('entrada', 'Entrada'),
+        ('plato_fuerte', 'Plato fuerte'),
+        ('postre', 'Postre'),
+        ('bebida', 'Bebida'),
+    ]
+
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    categoria = models.CharField(
+        max_length=20,
+        choices=CATEGORIAS,
+        default='plato_fuerte'
+    )
+    imagen = models.ImageField(
+        upload_to='platos/',
+        blank=True,
+        null=True
+    )
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
