96ac912310083ccc46686eada1c05ca4b45776b8
santiago <santiagoguzm2@gmail.com>
2026-09-22 07:21:01 -0500

    Implementar modelo Producto

--- las_ricuras/models.py
+++ las_ricuras/models.py
@@ -32,7 +32,43 @@ class Plato(models.Model):
     categoria = models.CharField(
         max_length=30,
         choices=CATEGORIAS,
-        default='platos_fuertes'
+        default='plato_fuerte'
+    )
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+
+    nombre = models.CharField(max_length=100)
+
+    descripcion = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    cantidad = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    unidad = models.CharField(
+        max_length=20
+    )
+
+    precio = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    disponible = models.BooleanField(
+        default=True
+    )
+
+    fecha_registro = models.DateTimeField(
+        auto_now_add=True
     )
 
     def __str__(self):
